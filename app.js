86c645253454f66bdbd4bf21b98ca9759141be7b
kareemah <ajimobikareemah@gmail.com>
2023-05-08 14:07:34 +0100

    product controller

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 
 const authRouter = require('./routes/authRoute');
 const userRouter = require('./routes/userRoute');
+const productRouter = require('./routes/productRoute');
 
 const notFoundMiddleware = require('./middleware/notfound');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -28,6 +29,7 @@ app.get('/', (req, res) => {
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
