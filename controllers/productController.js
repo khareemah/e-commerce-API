a34ed79986a757988c5722ae72f2726347fefc6c
kareemah <ajimobikareemah@gmail.com>
2023-05-09 09:02:01 +0100

    create review

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
@@ -5,6 +6,7 @@ const CustomError = require('../errors');
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
   const product = await Product.create(req.body);
+
   res.status(StatusCodes.CREATED).json({ product });
 };
 
@@ -43,11 +45,35 @@ const deleteProduct = async (req, res) => {
   if (!product) {
     throw new CustomError.NotFoundError(`No product with id ${productId}`);
   }
-  await product.remove();
+  await product.deleteOne();
   res.status(StatusCodes.OK).json({ msg: 'Success! Product removed.' });
 };
 
-const uploadImage = () => {};
+const uploadImage = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please upload image smaller than 1MB'
+    );
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
+};
 
 module.exports = {
   createProduct,
