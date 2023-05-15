64ed45dd2c065717504aca90e6cd116f04be13e9
kareemah <ajimobikareemah@gmail.com>
2023-05-15 21:36:00 +0100

    get all reviews associated with a single product

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -12,17 +12,22 @@ const {
   deleteProduct,
   uploadImage,
 } = require('../controllers/productController');
+const { getSingleProductReviews } = require('../controllers/reviewController');
 
 router
   .route('/')
   .post(authenticateUser, authorizePermissions('admin'), createProduct)
   .get(getAllProducts);
+
 router
   .route('/uploadImage')
   .post(authenticateUser, authorizePermissions('admin'), uploadImage);
+
 router
   .route('/:id')
   .get(getSingleProduct)
   .patch(authenticateUser, authorizePermissions('admin'), updateProduct)
   .delete(authenticateUser, authorizePermissions('admin'), deleteProduct);
+
+router.route('/:id/reviews').get(getSingleProductReviews);
 module.exports = router;
